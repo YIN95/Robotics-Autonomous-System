245612cc119da27712245f0fc07e768a937077fd
YIN95 <yinw@kth.se>
2018-09-09 04:22:51 +0200

    task1 [FINISH]

---
+++ ras_lab1_open_loop_control/src/open_loop_controller.cpp
@@ -0,0 +1,21 @@
+#include "ros/ros.h"
+#include "ras_lab1_msgs/PWM.h"
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "open_loop_controller");
+    ros::NodeHandle pwm_n;
+
+    ros::Publisher pwm_pub = pwm_n.advertise<ras_lab1_msgs::PWM>("/kobuki/pwm", 100);
+    ros::Rate loop_rate(10);
+
+    ras_lab1_msgs::PWM pwm_msg;
+
+    pwm_msg.PWM1 = -255;
+    pwm_msg.PWM2 = 255;
+
+    while (ros::ok()){
+        pwm_pub.publish(pwm_msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
